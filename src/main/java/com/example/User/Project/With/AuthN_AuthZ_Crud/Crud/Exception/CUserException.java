5795de0cf427c9cc221ed0b1d8c4de506e54186f
ParthCoder89 <parthgupta5041@gmail.com>
2026-02-13 22:37:47 +0530

    Commit the Exception Class of the crud Part of the project.

---
+++ src/main/java/com/example/User/Project/With/AuthN_AuthZ_Crud/Crud/Exception/CUserException.java
@@ -0,0 +1,7 @@
+package com.example.User.Project.With.AuthN_AuthZ_Crud.Crud.Exception;
+
+public class CUserException extends RuntimeException {
+    public CUserException(String message) {
+        super(message);
+    }
+}
