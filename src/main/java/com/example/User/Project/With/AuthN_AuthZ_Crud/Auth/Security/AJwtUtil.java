a95c5b451d55ddf778c5a988ec4ec5760bac99f2
ParthCoder89 <parthgupta5041@gmail.com>
2026-02-15 00:00:10 +0530

    Commit the jwtutil Class of the crud Part of the project.

---
+++ src/main/java/com/example/User/Project/With/AuthN_AuthZ_Crud/Auth/Security/AJwtUtil.java
@@ -0,0 +1,62 @@
+package com.example.User.Project.With.AuthN_AuthZ_Crud.Auth.Security;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+@RequiredArgsConstructor
+public class AJwtUtil {
+
+    private static final String SECRET = "mysecretkeymysecretkeymysecretkey123456";
+    private final Key key = Keys.hmacShaKeyFor(SECRET.getBytes());
+
+
+    public String generateToken(String userName, String role){
+           return Jwts.builder()
+                .signWith(key)
+                .setSubject(userName)
+                .claim("role", role)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .compact();
+    }
+
+    public String extractUsername(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public String extractRole(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .get("role", String.class);
+    }
+
+
+    public boolean validateToken(String token){
+        try{
+            Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch(JwtException | IllegalArgumentException e){
+            System.out.println("Invalid Jwt Token: " + e.getMessage());
+        }
+        return false;
+    }
+}
