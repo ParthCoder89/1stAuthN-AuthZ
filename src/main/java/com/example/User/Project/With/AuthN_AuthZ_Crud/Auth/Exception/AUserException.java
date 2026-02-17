42279a248850cd354e8c8ebfee05894dbee596ef
ParthCoder89 <parthgupta5041@gmail.com>
2026-02-17 23:17:36 +0530

    Commit the Admin User Exception class of the Auth Part of the project.

---
+++ src/main/java/com/example/User/Project/With/AuthN_AuthZ_Crud/Auth/Exception/AUserException.java
@@ -0,0 +1,7 @@
+package com.example.User.Project.With.AuthN_AuthZ_Crud.Auth.Exception;
+
+public class AUserException extends RuntimeException {
+    public AUserException(String message) {
+        super(message);
+    }
+}
